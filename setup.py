2ef60f21d0ef960f0f7ad2bf8e708d6c4d5c0779
Karl Berggren <kalle@jjabba.com>
2021-08-11 21:45:20 -0700

    Added rudiment support for pulling stability data.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysnag",
-    version="0.0.1",
+    version="0.0.2",
     author="Karl Berggren",
     author_email="kalle@jjabba.com",
     description="lib for easy access to bugsnags's APIv2",
